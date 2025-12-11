4eac734d1702974809b297ede0a1936a13f048df
singhHarcharan <harcharansingh198400@gmail.com>
2025-12-11 12:13:21 +0530

    Prisma Recap, Relationships in Prisma(How to add foreign key in DB and push data to DB using the concept of Relationships)

---
+++ 12.5.ORMs_Prisma_Joins/src/index.ts
@@ -0,0 +1,10 @@
+// Assignment : CRUD operations for To-Do application usign Prisma ORMs and any DB (neon, aiven etc.)
+
+// Here is the slide link you can use for reference : https://projects.100xdevs.com/tracks/gZf9uBBNSbBR7UCqyyqT/prisma-1
+
+// Conclusion : 
+// - Why ORMs
+// - Intro to Prisma
+// - Installing Prisma in Fresh Typescript Application
+// - Generating Prisma client to get access of table we create using prisma
+// - Relationships in Prisma
